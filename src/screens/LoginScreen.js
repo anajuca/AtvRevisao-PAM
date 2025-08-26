470b6a8139a9998ef7596909ad8c4b241c6569fb
Ana Júlia <anaju.aoliveiraa@gmail.com>
2025-08-26 19:49:37 -0300

    alterações tela 2

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
 import { Image } from 'react-native';
 import Checkbox from 'expo-checkbox';
+import Icon from 'react-native-vector-icons/Ionicons';
 import { View, Text, TouchableOpacity, StyleSheet, Dimensions, TextInput } from 'react-native';
 
 export default function LoginScreen({ navigation }) {
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
     const [lembrarSenha, setLembrarSenha] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
 
     return (
         <View style={styles.container}>
@@ -25,23 +31,38 @@ export default function LoginScreen({ navigation }) {
 
             <Text style={styles.label}>Senha</Text>
 
-            <TextInput
-                style={styles.input}
-                value={senha}
-                onChangeText={setSenha}
-                placeholder='Digite sua senha'
-                secureTextEntry={true}
-            />
+            <View style={styles.containerSenha}>
+
+                <TextInput
+                    style={styles.input}
+                    value={senha}
+                    onChangeText={setSenha}
+                    placeholder='Digite sua senha'
+                    secureTextEntry={!showPassword}
+
+                />
+
+                <TouchableOpacity onPress={togglePasswordVisibility} style={styles.iconButton}>
+                    <Icon
+                        name={showPassword ? 'eye-off-outline' : 'eye-outline'}
+                        size={24}
+                        color="#888"
+                    />
+                </TouchableOpacity>
+
+
+            </View>
+
 
             <View style={styles.checkboxContainer}>
                 <Checkbox
                     style={styles.checkbox}
                     value={lembrarSenha}
                     onValueChange={setLembrarSenha}
-                    color={lembrarSenha ? '#4630EB' : undefined}
+                    color={lembrarSenha ? '#14c871' : '#14c871'}
                 />
-                <Text style={styles.acbText}>Lembrar senha</Text>
-                <Text style={styles.acbText}>Esqueci minha senha</Text>
+                <Text style={styles.remind}>Lembrar senha</Text>
+                <Text style={styles.forgot}>Esqueci minha senha</Text>
             </View>
 
             <View style={styles.buttons}>
@@ -54,7 +75,11 @@ export default function LoginScreen({ navigation }) {
                 </TouchableOpacity>
             </View>
 
-            <Text style={styles.continue}>Ou continue com</Text>
+            <View style={styles.separator}>
+                <View style={styles.line}></View>
+                <Text style={styles.continue}>Ou continue com</Text>
+                <View style={styles.line}></View>
+            </View>
 
             <View style={styles.imgContainer}>
                 <Image source={require('../../assets/Google.png')} style={styles.icon} />
@@ -66,9 +91,11 @@ export default function LoginScreen({ navigation }) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 20,
-        paddingTop: 60,
+        padding: 15,
+        justifyContent: 'center',
+        alignItems: 'center',
         backgroundColor: '#f3f3f3',
+        marginBottom: 100
     },
     title: {
         fontSize: 34,
@@ -76,33 +103,35 @@ const styles = StyleSheet.create({
         marginTop: 35,
         color: '#555555',
         fontWeight: 470,
+        right: 115
     },
     subtitle: {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 12,
-        marginBottom: 35,
+        marginBottom: 20,
         color: '#555555',
-        fontWeight: 300,
+        fontWeight: 330,
+        right: 115
     },
     icon: {
-        width: 32,
-        height: 32,
+        width: 50,
+        height: 50,
+
     },
     imgContainer: {
-        width: 38,
-        height: 38,
-        position: 'absolute',
-        left: 15,
-        backgroundColor: '#fefefe',
-        borderRadius: 5,
-        padding: 3
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: "space-evenly",
+        alignItems: 'center',
+        marginTop: 25,
+        width: '65%',
     },
     acessButton: {
         backgroundColor: '#14c871',
         margin: 10,
         borderRadius: 10,
-        width: '50%',
+        width: 160,
         maxWidth: 350,
         height: 65,
         justifyContent: 'center',
@@ -116,11 +145,24 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         borderColor: '#14c871',
         borderWidth: 1,
-        width: 350,
+        width: 160,
         height: 65,
         padding: 10,
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    separator: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        marginTop: 40,
+        width: '90%',
+    },
+    line: {
+        height: 1,
+        backgroundColor: '#b6b6b6',
+        width: "30%"
     },
     acbText: {
         color: '#fefefe',
@@ -132,17 +174,26 @@ const styles = StyleSheet.create({
         fontSize: 12,
         fontWeight: 400,
     },
+    remind: {
+        color: '#555555',
+        fontSize: 12,
+        fontWeight: 350,
+        right: 35,
+        marginTop: 2,
+    },
+    forgot: {
+        color: '#555555',
+        fontSize: 12,
+        fontWeight: 350,
+        left: 40,
+        marginTop: 2,
+    },
     checkboxContainer: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        display: 'flex',
-        flexDirection: 'row'
+        flexDirection: 'row',
+        margin: 10
     },
     checkbox: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        display: 'flex',
-        flexDirection: 'row'
+        right: 45
     },
     buttons: {
         justifyContent: 'center',
@@ -152,22 +203,35 @@ const styles = StyleSheet.create({
     },
     label: {
         color: '#555555',
-        fontSize: 10,
+        fontSize: 12,
         fontWeight: 300,
+        right: 155,
+        marginTop: 10,
     },
     input: {
-        backgroundColor: '#bec3c7',
+        backgroundColor: '#dbdbdb',
         margin: 10,
+        marginTop: 5,
         borderRadius: 10,
         width: '100%',
         maxWidth: 350,
         height: 65,
         display: 'flex',
-        flexDirection: 'row'
+        flexDirection: 'row',
+        paddingLeft: 15
+    },
+    iconButton: {
+        right: 50
+    },
+    containerSenha: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        left: 10
     },
     continue: {
         color: '#6d6d6d',
         fontSize: 12,
         fontWeight: 350,
-    }
+    },
 });
